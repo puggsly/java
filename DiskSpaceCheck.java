c990023fa91e1a0ab035aa4b925a785ab1aa32a2
puggsly <tespos58per@yahoo.com>
2015-11-29 16:47:02 -0600

    Initial version

---
+++ DiskSpaceCheck.java
@@ -0,0 +1,25 @@
+//
+//	  File:  DiskSpaceCheck.java
+//    Purpose:  Check disk space on Linux file space
+//
+//	  History
+//    Date 		Author			Remarks
+/	  20151125	T. Esposito		original version
+//
+
+import java.io.File;
+
+public class DiskSpaceCheck
+{
+    public static void main(String[] args)
+        {
+    	File file = new File("c:");
+	   	long totalSpace = file.getTotalSpace(); //total disk space in bytes.
+	   	long usableSpace = file.getUsableSpace(); ///unallocated / free disk space in bytes.
+	   	long freeSpace = file.getFreeSpace(); //unallocated / free disk space in bytes.
+
+        System.out.println("Total size : " + totalSpace /1024 /1024 + " MB");
+		System.out.println("Space free : " + usableSpace /1024 /1024 + " MB");
+		System.out.println("Space free : " + freeSpace /1024 /1024 + " MB");
+	    }
+}
